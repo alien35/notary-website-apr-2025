4510f4850ff677cc0fc3579e015f8d71a195132d
Alexander Leon <aleon6@u.rochester.edu>
2025-04-07 15:48:17 -0400

    w

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,27 +1,13 @@
-import { defineSchema } from 'sanity'
-
-import { blockContent } from './schemaTypes/blockContent'
-import { category } from './schemaTypes/category'
-import { post } from './schemaTypes/post'
-import { author } from './schemaTypes/author'
+import {blockContent} from './schemaTypes/blockContent'
+import {category} from './schemaTypes/category'
+import {post} from './schemaTypes/post'
+import {author} from './schemaTypes/author'
 import conditionalText from './schemaTypes/conditionalText'
 import { table } from './schemaTypes/table'
-import { worksOnDevices } from './schemaTypes/worksOnDevices'
+import {worksOnDevices} from './schemaTypes/worksOnDevices'
 import statePicker from './schemaTypes/statePicker'
-import { breadcrumbs } from './schemaTypes/breadcrumbs'
-// Add others here (e.g., youtube, countryAndRegionPicker, etc.)
+import {breadcrumbs} from './schemaTypes/breadcrumbs';
 
-export const schema = defineSchema({
-  types: [
-    post,
-    author,
-    category,
-    blockContent,
-    conditionalText,
-    table,
-    worksOnDevices,
-    statePicker,
-    breadcrumbs,
-    // Add missing custom blocks here
-  ],
-})
+export const schema = {
+  types: [post, author, category, blockContent, conditionalText, table, worksOnDevices, statePicker, breadcrumbs],
+}
