1bc457bfb5e6aa9837c8242e80ff37f6214a8b5a
Alexander Leon <aleon6@u.rochester.edu>
2025-06-28 22:20:46 -0400

    w

--- app/signing-services/page.tsx
+++ app/signing-services/page.tsx
@@ -16,10 +16,7 @@ export default function SigningServicesPage() {
 
       <div className="prose max-w-none text-muted-foreground mb-8">
         <p>
-          I've compiled a list of Signing Services, Title Companies, Attorneys, Field Inspector Companies, and others who use notaries. I've tried to update the list to remove links that are now out of business. You must do your due diligence when signing up with any of these companies and others. This is one of the lists I give to notaries I've trained and gotten into the notary business.
-        </p>
-        <p>
-          I'm leaving this public on my website blog so other notaries can access this information for FREE! I'm not asking you to "DM" me on Facebook or join my email list to have access to this information. This is a list of over 100 contacts compiled for you and costing you absolutely nothing! It's up to you to submit your information to the companies you choose!
+          We've compiled a list of Signing Services, Title Companies, Attorneys, Field Inspector Companies, and others who use notaries. I've tried to update the list to remove links that are now out of business. You must do your due diligence when signing up with any of these companies and others. This is one of the lists I give to notaries I've trained and gotten into the notary business.
         </p>
         <p>
           Before submitting your information to signing services and title companies, you should organize a dedicated file folder on your computer containing all your credentials. This will ensure convenient access to the necessary information when or if it's asked for. Among the credentials to include in your folder are:
@@ -63,7 +60,7 @@ export default function SigningServicesPage() {
         </p>
         <p>
           <em>
-            *If you're a notary and have been hired by any of these companies and have <strong>NOT</strong> been paid by them or payment was over 30 days - please email me the company's name and info so I can take them off this list! Please be aware that some companies on this list may be low-payers, that's up to you and your business to negotiate your acceptable fee! I'm only interested in non-payers and payers over 30 days (unless the contract you signed with that company is different - that's on you, also). Email: <a href="mailto:info@SmokyMtnNotary.com">info@SmokyMtnNotary.com</a>
+            *If you're a notary and have been hired by any of these companies and have <strong>NOT</strong> been paid by them or payment was over 30 days - please email me the company's name and info so I can take them off this list! Please be aware that some companies on this list may be low-payers, that's up to you and your business to negotiate your acceptable fee! I'm only interested in non-payers and payers over 30 days (unless the contract you signed with that company is different - that's on you, also). Email: <a href="mailto:info@SmokyMtnNotary.com">alex@notarycentral.org</a>
           </em>
         </p>
       </div>
